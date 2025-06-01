9d0ad98ad72845263f97e9eb1e879bd6a24004b0
vvsafwan <safdev1820@gmail.com>
2025-06-01 14:02:29 +0530

    contact map updation

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -25,7 +25,6 @@ export default function Contact() {
 
                 <div className="w-full py-10 filter grayscale">
                      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3913.019462796582!2d75.76654207584217!3d11.259978350086733!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba6592a367c97f9%3A0xc7d6a987916777f3!2sWHAC%20what%20a%20content!5e0!3m2!1sen!2sin!4v1748724665105!5m2!1sen!2sin" width="100%" height="300" style={{border: '0'}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-                     <div className="absolute top-10 left-0 w-full h-[300px] bg-black opacity-30 pointer-events-none"></div>
                 </div>
 
 
